250748244f7c75bb7113b6ba59cc0e7960a6f7a6
Rohit <tprohit9@gmail.com>
2021-05-17 20:39:07 +0530

    Implemented state selection.

--- src/register.js
+++ src/register.js
@@ -17,20 +17,86 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
  */
 
-// eslint-disable-next-line no-unused-vars
-import {Client, Message} from "discord.js";
+import Discord from "discord.js";
+import fetch from "node-fetch";
+
+const Texts = 
+{
+	helpInfo: "Don't worry you can always ask for !help 😉",
+	ageError: "Did you really forget your age, or are you trolling me 🤔.",
+	sateQuery: 
+	[ 
+		"Select The Sate", 
+		"Select the state from where you wan't to get vaccinated. Don't worry you can change it latter." 
+	]
+};
+
+const fetchOptions = { 
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		"Accept-Language": "hi_IN",
+		Host: "cdn-api.co-vin.in",
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+	}
+};
+
+/**
+ * @author Rohit T P
+ * @param {string} title
+ * @param {string} description
+ * @param {any[]} options
+ * @returns {Discord.MessageEmbed}
+ */
+function getEmbeding(title, description, options) 
+{
+	const fields = options.map((option) => 
+		({ name: option.name, value: option.id, inline: true }));
+
+	return new Discord.MessageEmbed()
+		.setColor("#0099ff")
+		.setTitle(title)
+		.setDescription(description)
+		.addFields(fields)
+		.setTimestamp()
+		.setFooter("Reply within 1 minute");
+}
 
 /**
  * This function handles the user registration for Punarjani.
  * 
  * @author Rohit T P
- * @param {Client} client The discord client
- * @param {Message} message The message that initiated this command.
- * @param {Array<string>} args Array containing district, age and user id.
- * @returns {Boolean} Indicates operation success or failure.
+ * @param {Discord.Client} client The discord client
+ * @param {Discord.Message} message The message that initiated this command.
+ * @param {Array<string>} args Array containing age.
+ * @returns {Promise<Boolean>} Indicates operation success or failure.
  */
-export default function register(client, message, args) 
+export default async function register(client, message, args) 
 {
-	console.log(args);
+	if(!(Number(args[0]) >= 18))
+		return message.reply(`${Texts.ageError}\n${Texts.helpInfo}`), false;
+
+	const response = await fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states", fetchOptions)
+		.then((res) => res.json());
+	
+	const states = response.states
+		.map((/** @type {any} */ state) => ({id: state.state_id, name: state.state_name}));	
+		
+	const embeding = getEmbeding(Texts.sateQuery[0], Texts.sateQuery[1], states);	
+	const reply = await message.channel.send(embeding);
+
+	// @ts-ignore
+	const numberFilter = (response) => !isNaN(response.content);
+
+	const choise = await reply.channel.awaitMessages(numberFilter, {max: 1, errors: ["time"]})
+		.then((collected) => [ Number(collected.first()?.content), collected.first()?.author ])
+		.catch(() => [NaN, {}]);
+
+	const state = states.find((/** @type {{ id: number; }} */ state) => state.id === choise[0])?.name;	
+
+	// @ts-ignore
+	reply.channel.send(`@${choise[1]?.username} selected ${state}`);
+
 	return true;
 }
+
