8a8515947bee1e526c62d94ec12f80cd8773bd31
Rohit <tprohit9@gmail.com>
2021-05-18 15:39:49 +0530

    Completed state and district selection

--- src/register.js
+++ src/register.js
@@ -1,5 +1,5 @@
 /**
- * Punarjani is a discord bot that notifies you about slot availablity at
+ * Punarjani is a discord bot that notifies you about slot availability at
  * CoWin vaccination centers.
  * Copyright (C) 2021  Rohit T P
  * 
@@ -18,48 +18,130 @@
  */
 
 import Discord from "discord.js";
-import fetch from "node-fetch";
+import {sendRequest, getApp} from "./common.js";
 
 const Texts = 
 {
 	helpInfo: "Don't worry you can always ask for !help 😉",
-	ageError: "Did you really forget your age, or are you trolling me 🤔.",
-	sateQuery: 
+	tryAgain: "Don't worry you can try again later 🤘",
+	infoCollected: "That's I need to know 👍 I will write it in my 📖 and let you know.",
+	stateQuery: 
 	[ 
 		"Select The Sate", 
-		"Select the state from where you wan't to get vaccinated. Don't worry you can change it latter." 
-	]
-};
-
-const fetchOptions = { 
-	method: "GET",
-	headers: {
-		accept: "application/json",
-		"Accept-Language": "hi_IN",
-		Host: "cdn-api.co-vin.in",
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-	}
+		"Select the state from where you want to get vaccinated. Don't worry you can change it latter." 
+	],
+	districtQuery:
+	[
+		"Select The District",
+		"Select your preferred district. Only showing districts from the state you selected."
+	],
+	ageError: "Did you really forget your age, or are you trolling me 🤔",
+	stateError: "Registration has been canceled due to invalid state selection 😭",
+	districtError: "Registration has been canceled due to invalid district selection 😭"
 };
 
 /**
+ * Helper function to create embed from passed data.
+ * 
  * @author Rohit T P
- * @param {string} title
- * @param {string} description
- * @param {any[]} options
- * @returns {Discord.MessageEmbed}
+ * @param {string} title The title of the embed.
+ * @param {string} description The description of the embed.
+ * @param {string} avatar The url to an image to use as avatar.
+ * @param {{name: string, id: number}[]} options An object containing options to list.
+ * @returns {Discord.MessageEmbed[]} The embed created using given data.
  */
-function getEmbeding(title, description, options) 
+function getEmbeds(title, description, avatar, options) 
 {
+	const embeds = [];
 	const fields = options.map((option) => 
-		({ name: option.name, value: option.id, inline: true }));
+		({ name: `${option.name} - ${option.id}`, value: "_", inline: true }));
+	
+	while(fields.length)
+		embeds.push(new Discord.MessageEmbed()
+			.setThumbnail(avatar)
+			.setColor("#0099ff")
+			.addFields(fields.splice(0, 25)));
+	
+	embeds[0].setTitle(title)
+		.setDescription(description);
+	
+	embeds[embeds.length-1].setTimestamp()
+		.setFooter("Reply within 1 minute", avatar); 	
+		
+	return embeds;	
+}
 
-	return new Discord.MessageEmbed()
-		.setColor("#0099ff")
-		.setTitle(title)
-		.setDescription(description)
-		.addFields(fields)
-		.setTimestamp()
-		.setFooter("Reply within 1 minute");
+/**
+ * A helper function to show a list of states and let the user pick one.
+ * 
+ * @param {Discord.User} user The author of message.
+ * @param {Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel} channel The message channel.
+ * 
+ * @returns {Promise<{name: string, id: number}>} The selected state
+ */
+async function getState(user, channel) 
+{
+	// Get the list of state name and id from cowin API.
+	const response = await sendRequest("https://cdn-api.co-vin.in/api/v2/admin/location/states");
+
+	const states = response.states // Format the response we got from the API to an object array.
+		.map((/** @type {any} */ state) => ({id: state.state_id, name: state.state_name}));	
+	
+	// Create embeds with the state list and user's avatar and send it.	
+	const embeds = getEmbeds(Texts.stateQuery[0], Texts.stateQuery[1], user.displayAvatarURL(), states);	
+	embeds.forEach(async (embed) =>await channel.send(embed));
+
+	// Define a function to filter the replys from the user.
+	const numberFilter = (/** @type {Discord.Message} */ response) => 
+	// Check if the reply is from the correct user and also a valid option. 
+		response.author.id === user.id && !isNaN(Number(response.content));
+
+	// Wait for the user to reply with the code of state they want to select.	
+	const choice = await channel.awaitMessages(numberFilter, {max: 1, errors: ["time"]})
+		.then((collected) => Number(collected.first()?.content));
+
+	// Search the states array for the state specified by the state code user sent. 	
+	const state = states.find((/** @type {{ id: number; }} */ state) => state.id === choice);	
+
+	// Send an appropriate reply to the user.
+	channel.send(`<@${user.id}> You selected ${state ? state.name : "an invalid state"}`);
+	
+	return state;
+}
+
+/**
+ * @param {Discord.User} user
+ * @param {Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel} channel
+ * @param {{ name: any; id?: number; }} state
+ */
+async function getDistrict(user, channel, state) 
+{
+	// Get the list of district name and id from cowin API.
+	const response = await sendRequest(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${state.id}`);
+
+	const districts = response.districts // Format the response we got from the API to an object array.
+		.map((/** @type {any} */ dist) => ({id: dist.district_id, name: dist.district_name}));	
+	
+	// Create embeds with the district list and user's avatar and send it.	
+	const embeds = getEmbeds(Texts.districtQuery[0], Texts.districtQuery[1], user.displayAvatarURL(), districts);	
+	embeds.forEach(async (embed) =>await channel.send(embed));
+
+	// Define a function to filter the replies from the user.
+	const numberFilter = (/** @type {Discord.Message} */ response) => 
+	// Check if the reply is from the correct user and also a valid option. 
+		response.author.id === user.id && !isNaN(Number(response.content));
+
+	// Wait for the user to reply with the code of district they want to select.	
+	const choice = await channel.awaitMessages(numberFilter, {max: 1, errors: ["time"]})
+		.then((collected) => Number(collected.first()?.content));
+
+	// Search the districts array for the code user sent. 	
+	const district = districts.find((/** @type {{ id: number; }} */ state) => state.id === choice);	
+
+	// Send an appropriate reply to the user.
+	channel.send(`<@${user.id}> You selected ${district ? district.name : "an invalid district"}`);
+	
+	return district;
 }
 
 /**
@@ -73,29 +155,35 @@ function getEmbeding(title, description, options)
  */
 export default async function register(client, message, args) 
 {
-	if(!(Number(args[0]) >= 18))
-		return message.reply(`${Texts.ageError}\n${Texts.helpInfo}`), false;
+	// This check would accept 18.123 but I think it is feature not a bug.
+	if(!(Number(args[0]) >= 18)) // Check if the arguments passed contains a valid age.
+		return message.reply(`${Texts.ageError}\n${Texts.helpInfo}`), false;	
 
-	const response = await fetch("https://cdn-api.co-vin.in/api/v2/admin/location/states", fetchOptions)
-		.then((res) => res.json());
+	// Get the state name and ID of the user.	
+	const state = await getState(message.author, message.channel);
+
+	if(!state) // Check if the user selected a valid state.
+		return message.reply(`${Texts.stateError}\n${Texts.tryAgain}`), false;	
 	
-	const states = response.states
-		.map((/** @type {any} */ state) => ({id: state.state_id, name: state.state_name}));	
-		
-	const embeding = getEmbeding(Texts.sateQuery[0], Texts.sateQuery[1], states);	
-	const reply = await message.channel.send(embeding);
+	const district = await getDistrict(message.author, message.channel, state);	
 
-	// @ts-ignore
-	const numberFilter = (response) => !isNaN(response.content);
+	if(!district) // Check if the user selected a valid state.
+		return message.reply(`${Texts.districtError}\n${Texts.tryAgain}`), false;
 
-	const choise = await reply.channel.awaitMessages(numberFilter, {max: 1, errors: ["time"]})
-		.then((collected) => [ Number(collected.first()?.content), collected.first()?.author ])
-		.catch(() => [NaN, {}]);
+	// Send a message to the user that data collection has been completed.	
+	message.reply(Texts.infoCollected);
+	
+	// Get an instance of firebase app to write data to database.
+	const app = getApp();
 
-	const state = states.find((/** @type {{ id: number; }} */ state) => state.id === choise[0])?.name;	
-
-	// @ts-ignore
-	reply.channel.send(`@${choise[1]?.username} selected ${state}`);
+	app.firestore().collection("users").add(
+		{
+			userID: message.author.id,
+			userName: message.author.username,
+			age: Number(args[0]),
+			district,
+			state
+		});
 
 	return true;
 }
