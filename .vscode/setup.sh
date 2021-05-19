01489e4a5a471831e8f5db6ca6020368ede2be9d
Rohit <tprohit9@gmail.com>
2021-05-19 12:34:57 +0530

    Fixed setup bug

--- .vscode/setup.sh
+++ .vscode/setup.sh
@@ -1,5 +1,6 @@
 git switch main
 git fetch -p
+git branch | grep -v "main" | xargs git branch -D || echo "No branch to delete" 
 git pull
 git checkout $(git show-ref --verify --quiet refs/heads/$USER || echo '-b') $USER
 git push -u origin $USER && npm i
