a99d0d1a317ae2ed833eb3c0f4f85db2b790eb91
Rohit <tprohit9@gmail.com>
2021-06-05 14:26:20 +0530

    Fixed eslint errors

--- src/common.js
+++ src/common.js
@@ -69,15 +69,16 @@ export async function getSessions(id, date, cache)
 {
 	const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`;
 	const result = await axios.get(url,
-                       {
-                             headers: {
+		{
+			headers: 
+			{
 				accept: "application/json",
 				"Accept-Language": "hi_IN",
 				Host: "cdn-api.co-vin.in",
 				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-			     } 
-                       })
-                        .catch(() =>undefined);
+			} 
+		})
+		.catch(() =>undefined);
 
 	let data = result?.data;
 
