b265c4b95b3f417a5f6a00122b131975b581ecf0
Rohit <tprohit9@gmail.com>
2021-06-07 01:41:56 +0530

    bug fixes

--- docs/sw.js
+++ docs/sw.js
@@ -1,44 +1,36 @@
-const VERSIONED_CACHE = "punarjani-cache-v3";
+const VERSIONED_CACHE = "punarjani-cache-v4";
 const PERSISTENT_CACHE = "persistent-cache-punarjani";
 
 const versioned_urls = [
 	"/",
-	"./assets/css/style.css",
-	"./assets/js/main.js",
-	"./assets/vendor/aos/aos.css",
-	"./assets/vendor/aos/aos.js",
-	"./assets/vendor/bootstrap/css/bootstrap-icons.css",
-	"./assets/vendor/bootstrap/css/bootstrap.min.css",
-	"./assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
-	"./assets/vendor/glightbox/js/glightbox.min.js",
-	"./assets/vendor/isotope-layout/isotope.pkgd.min.js",
-	"./assets/vendor/swiper/swiper-bundle.min.css",
-	"./assets/vendor/swiper/swiper-bundle.min.js",
+	"/assets/css/style.css",
+	"/assets/js/main.js",
+	"/assets/vendor/aos/aos.css",
+	"/assets/vendor/aos/aos.js",
+	"/assets/vendor/bootstrap/css/bootstrap-icons.css",
+	"/assets/vendor/bootstrap/css/bootstrap.min.css",
+	"/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+	"/assets/vendor/glightbox/js/glightbox.min.js",
+	"/assets/vendor/isotope-layout/isotope.pkgd.min.js",
+	"/assets/vendor/swiper/swiper-bundle.min.css",
+	"/assets/vendor/swiper/swiper-bundle.min.js",
 ];
 
 const persistent_urls = [
-	"./assets/fonts/boxicons.woff2",
-	"./assets/fonts/bootstrap-icons.woff2",
-	"./assets/img/Working/edit.jpg",
-	"./assets/img/Working/help.png",
-	"./assets/img/Working/info.png",
-	"./assets/img/Working/profile.jpg",
-	"./assets/img/Working/register.jpg",
-	"./assets/img/Working/slots.jpg",
-	"./assets/img/developers/Rohit.jpg",
-	"./assets/img/developers/sanu.jpg",
-	"./assets/img/developers/Sunith.jpg",
-	"./assets/img/logos/logo-192.png",
-	"./assets/img/logos/logo-48.png",
-	"./assets/img/logos/logo-512.png",
-	"./assets/img/steps/step1.jpg",
-	"./assets/img/steps/step2.jpg",
-	"./assets/img/steps/step3.jpg",
-	"./assets/img/steps/step4.jpg",
-	"./assets/img/counts-bg.png",
-	"./assets/img/features.png",
-	"./assets/img/footer-bg.jpg",
-	"./assets/img/hero-bg.jpg"
+	"/assets/fonts/boxicons.woff2",
+	"/assets/fonts/bootstrap-icons.woff2",
+	"/assets/img/Working/edit.jpg",
+	"/assets/img/Working/info.png",
+	"/assets/img/Working/profile.jpg",
+	"/assets/img/Working/register.jpg",
+	"/assets/img/Working/slots.jpg",
+	"/assets/img/logos/logo-192.png",
+	"/assets/img/logos/logo-48.png",
+	"/assets/img/logos/logo-512.png",
+	"/assets/img/counts-bg.png",
+	"/assets/img/features.png",
+	"/assets/img/footer-bg.jpg",
+	"/assets/img/hero-bg.jpg"
 ];
 
 async function cacheAll(name, urls) 
